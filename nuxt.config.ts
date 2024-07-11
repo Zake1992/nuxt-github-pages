31494565c24ffe467bec5eb1bc182a4cb2734b1f
张昆 <zhangkun@wearlab.tech>
2024-07-11 10:19:52 +0800

    提交docs目录

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
+const path = require('path');
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  nitro: { output:{
+    publicDir: path.join(__dirname, 'docs'),
+  }},
 })
